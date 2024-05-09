c45dd2e84af26abf0c443e7348591e254148b74e
GreenRoot <1.ooo.ooo@bk.ru>
2024-05-10 02:29:31 +0300

    Add move logic and display value on screen

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 const game = {
-  players: ['player1', 'player2'],
-  steps: [
-    { id: 1, dice1: 1, dice2: 1 },
-    { id: 2, dice1: 2, dice2: 2 },
-    { id: 3, dice1: 3, dice2: 3 },
-  ]
+  players: ['Батон', 'Доброжир'],
+  steps: [{ dice1: 0, dice2: 0 }]
+}
+
+const dices = document.querySelector('.dice-wrapper')
+const reset = document.querySelector('.reset')
+
+dices.addEventListener('click', (e) => {
+  const dice1 = Math.floor(Math.random() * 6) + 1
+  const dice2 = Math.floor(Math.random() * 6) + 1
+  game.steps.push({ dice1, dice2 })
+  dicesRender()
+  console.log(game)
+})
+
+reset.addEventListener('click', (e) => {
+  game.steps = [{ dice1: 0, dice2: 0 }]
+  dicesRender()
+})
+
+function dicesRender() {
+  const dices = document.querySelector('.dice-wrapper')
+  dices.innerHTML = `
+    <div class="dice">
+        ${game.steps[game.steps.length - 1].dice1}
+    </div>
+    <div class="dice">
+        ${game.steps[game.steps.length - 1].dice2}
+    </div>
+  `
 }
